eedf8c3cec08a5f1c195b4124f6eb170e83e1051
Edgar M <meling.edgar@gmail.com>
2025-10-29 18:38:18 +0100

    Kopplung und Bindung angefangen

---
+++ SWE/Aufgabenblatt_2/1Kopplung_Bindungerstellen/src/DatenKop.java
@@ -0,0 +1,18 @@
+public class DatenKop {
+
+public int zahl (int a, int b){
+
+return a+b;
+}
+
+
+public static void main(String[] args) {
+
+    DatenKop abc = new DatenKop();
+    System.out.println(abc.zahl(2, 2));
+    
+}
+
+
+
+}
